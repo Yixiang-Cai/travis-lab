41ee29a71e466c709824f590fb130fc18e3daf3a
Dailey <yc3364@columbia.edu>
2018-01-27 10:32:13 -0500

    test11

--- task2/test_3.py
+++ task2/test_3.py
@@ -2,7 +2,7 @@
 import re
 
 def test_answer(file='input.txt'):
-    f = open(file,'r',encoding='utf-8')
+    f = open(file,'r')
     words = ''.join(f.readlines())
     regex_str = re.compile("[\u2E00-\uFFA5]+")
     assert len(''.join(regex_str.findall(words)))==6
