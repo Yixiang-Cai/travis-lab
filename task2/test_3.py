6dc948fd26724dfa539e97a6f87741a622ec7f85
Dailey <yc3364@columbia.edu>
2018-01-27 11:47:23 -0500

    test14

--- task2/test_3.py
+++ task2/test_3.py
@@ -5,9 +5,9 @@ minimum=ord(u'\u2E00')
 maximum=ord(u'\uFFA5')
 
 def read_input(file=file,minimum=minimum):
-    f=open(file,'r')
+    f=open(file,'r',encoding='utf-8')
     text=f.read()
-    result=''.join([tt[i] for i in range(len(tt)) if (ord(tt[i])>minimum)])
+    result=''.join([text[i] for i in range(len(text)) if (ord(text[i])>minimum)])
     return result
 
 def test_answer():
