4a655337e8323d11b8d95c43922552b7c17728b2
Dailey <yc3364@columbia.edu>
2018-01-27 10:18:39 -0500

    test8

--- task2/test_1.py
+++ task2/test_1.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+def test_1():
+    assert 2/8==0.25
+
 def test_2():
     assert np.array([2/8])==np.array([0.25])
 
-def test_1():
-    assert 2/8==0.252
 
 
--- task2/test_2.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def test():
-    assert np.array([2/8])==np.array(0.252)
