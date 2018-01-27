192d9be4cccc13a1fb23540d3d96f2ee522901eb
Dailey <yc3364@columbia.edu>
2018-01-27 11:32:43 -0500

    test13

--- task2/test_3.py
+++ task2/test_3.py
@@ -4,7 +4,7 @@ file='input.txt'
 minimum=ord(u'\u2E00')
 maximum=ord(u'\uFFA5')
 
-def read_input(file=file,minimum=minimum)
+def read_input(file=file,minimum=minimum):
     f=open(file,'r')
     text=f.read()
     result=''.join([tt[i] for i in range(len(tt)) if (ord(tt[i])>minimum)])
