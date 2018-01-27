95354c0d368573e006c67e613779d4a0989048ca
Dailey <yc3364@columbia.edu>
2018-01-27 11:55:16 -0500

    test15

--- task2/test_3.py
+++ task2/test_3.py
@@ -1,14 +1,15 @@
-#coding=utf-8
+#-*- coding:utf-8 -*-
 
 file='input.txt'
 minimum=ord(u'\u2E00')
 maximum=ord(u'\uFFA5')
 
 def read_input(file=file,minimum=minimum):
-    f=open(file,'r',encoding='utf-8')
+    f=open(file,'r')
     text=f.read()
     result=''.join([text[i] for i in range(len(text)) if (ord(text[i])>minimum)])
     return result
 
 def test_answer():
     assert len(read_input())==6
+
